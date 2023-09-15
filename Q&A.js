a6175e40c3ce5c6799633fc50c710c5fb91d810a
Bhavya Kulshreshta Arya <119723974+BhavyaKulshreshta@users.noreply.github.com>
2023-09-15 23:33:35 +0530

    Add files via upload

---
+++ Q&A.js
@@ -0,0 +1,15 @@
+document.getElementById("surveyForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const q1Value = parseInt(
+    document.querySelector('input[name="q1"]:checked').value
+  );
+  const q2Value = parseInt(
+    document.querySelector('input[name="q2"]:checked').value
+  );
+  const q3Value = parseFloat(document.querySelector('input[name="q3"]').value);
+
+  const totalScore = q1Value + q2Value + q3Value;
+
+  alert(`Your total score is: ${totalScore}`);
+});
